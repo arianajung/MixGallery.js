23b7969b14851ec45f7f06aaae63c725b83c1d82
Ariana Jung <syjung0904@gmail.com>
2020-12-19 15:08:09 -0500

    Added comments

--- server.js
+++ server.js
@@ -22,5 +22,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     log(`Listening on port ${port}...`);
 }); // localhost development port 5000  (http://localhost:5000)
-// We've bound that port to localhost to go to our express server.
-// Must restart web server when you make changes to route handlers.
